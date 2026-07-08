6a252861ff4b6965589b4f60517cc4f1621b7b6a
Sandy Oliveira <38080685+SandyOliveira@users.noreply.github.com>
2026-07-08 17:18:54 -0300

    responsividade do cenario

--- main.js
+++ main.js
@@ -1,28 +1,108 @@
-
-var back,imgBack;
-var space;
 var enemyOne,meteoro;
-function preload(){
-    imgBack = loadImage("./assets/background.jpeg");
-    meteoro = loadImage("./assets/meteor.png");
+var imgSpace;
+var imgGround;
+
+var bgY1;
+var bgY2;
+
+var bgHeight;
+var bgSpeed = 1;
+var bgOffset = -400;
+
+function preload() {
+
+  imgSpace = loadImage("./assets/space3.png");
+  imgGround = loadImage("./assets/ground.png");
+  meteoro = loadImage("./assets/meteor.png");
 
 }
 
-function setup(){
-    createCanvas(windowWidth,windowHeight);
-    back = createSprite(width/2,height/2-200);
-    back.addImage(imgBack);
-   
+function setup() {
+
+  createCanvas(windowWidth, windowHeight);
+
+  adjustBackground();
+
 }
-function draw(){
-    background(220);
-    enemys();
-    drawSprites();
+
+function draw() {
+
+ background(0);
+
+  drawBackground();
+
+  enemys();
+
+  drawSprites();
+
+  drawGround();
+
 }
 
 function enemys(){
  if(frameCount%60===0){
     enemyOne = createSprite(50,0);
+    enemyOne.addImage(meteoro);
     enemyOne.velocityY = 4;
  }
+}
+
+//CENARIO - BACK AND SPACE
+function drawBackground() {
+
+  image(imgSpace, 0, bgY1 + bgOffset, width, bgHeight);
+  image(imgSpace, 0, bgY2 + bgOffset, width, bgHeight);
+
+  bgY1 += bgSpeed;
+  bgY2 += bgSpeed;
+
+  if (bgY1 >= bgHeight + 400) {
+    bgY1 = bgY2 - bgHeight;
+  }
+
+  if (bgY2 >= bgHeight + 400) {
+    bgY2 = bgY1 - bgHeight;
+  }
+
+}
+
+function drawGround() {
+
+  var scale = width / imgGround.width;
+  var groundHeight = imgGround.height * scale;
+
+  var groundOffset;
+
+  if (windowWidth > windowHeight) {
+    //mexe aqui Pedro pra descer o solo no modo web
+    groundOffset = 380;
+  } else {
+    groundOffset = 20;
+  }
+
+  image(
+    imgGround,
+    0,
+    height - groundHeight + groundOffset,
+    width,
+    groundHeight
+  );
+
+}
+
+function adjustBackground() {
+
+  bgHeight = imgSpace.height * (width / imgSpace.width);
+
+  bgY1 = 0;
+  bgY2 = -bgHeight;
+
+}
+
+function windowResized() {
+
+  resizeCanvas(windowWidth, windowHeight);
+
+  adjustBackground();
+
 }
